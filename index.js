982ec0d99e479dd3ba91808e80b41c4d93387c53
Rodgers Kaunda <rodgerskaunda@Rodgerss-MacBook-Pro.local>
2022-08-14 12:26:56 +0100

    finished project

--- index.js
+++ index.js
@@ -7,96 +7,106 @@ let form = document.querySelector(".display-form");
 console.log(container);
 
 class Book {
-    constructor(author,title,pages){
-        this.author=author;
+    constructor(author, title, pages) {
+        this.author = author;
         this.title = title;
         this.pages = pages
     }
-    
+
+
 }
 
 
 
 class Library {
-    constructor(arr =[]){
-        this.arr =arr;
+    constructor(arr = []) {
+        this.arr = arr;
         this.book = null;
     }
 
-    addBookToLibrary(){
-        let {arr,book,renderCard} = this;
-        form.addEventListener("click",function(e){
+    addBookToLibrary() {
+        let { arr, book, renderCard, changeStatus } = this;
+        form.addEventListener("click", function (e) {
             e.preventDefault();
-            if(e.target.matches("#submit")){
+            if (e.target.matches("#submit")) {
                 let authorValue = document.querySelector("#author").value;
                 let titleValue = document.querySelector("#title").value;
                 let pageValue = document.querySelector("#pages").value;
-                book = new Book(authorValue,titleValue,pageValue);
+                book = new Book(authorValue, titleValue, pageValue);
                 arr.push(book);
                 form.classList.toggle("display-form");
                 renderCard(book);
+
             }
         })
     }
 
-    renderCard(book){
-        let{author,title,pages} = book;
+    renderCard(book) {
+        let { author, title, pages, } = book;
         let createDiv = document.createElement("div");
-        createDiv.innerHTML = `<h1>${author}</h1>`;
+        createDiv.innerHTML = `<span></span><h1>${title}</h1><h1>${author}</h1><h1>${pages}</h1>
+        <i class="fa fa-remove"></i>
+        <button type="button">Click Me!</button><label for="status">read</label><input type="checkbox" id="status" name="status" >
+        `;
         createDiv.classList.add("card");
         container.appendChild(createDiv);
 
 
     }
 
-  
+    deleteBook() {
+        let arr = this.arr;
+        container.addEventListener("click", function (event) {
+            if (event.target.matches(".fa-remove")) {
+                let child = event.target.parentNode;
+                let sibling = event.target.previousSibling.innerText;
+                for (let i = 0; i < arr.length; i++) {
+                    let book = arr[i];
+                    if (book.author === sibling) {
+                        arr.pop()
+                    }
+                }
+                container.removeChild(child);
+
+            }
+        })
+
+    }
+
+    changeStatus() {
+        container.addEventListener("click", function (event) {
+        
+           
+            
+if (event.target.matches("button")) {
+                let checkbox = event.target.nextElementSibling.nextElementSibling;
+                if(!checkbox.checked){
+                    checkbox.checked = true;
+                }else{
+                    checkbox.checked = false;
+                }
+            }
+
+        })
+
+    }
+
+
+
+
 }
 
 
 let lib = new Library();
 lib.addBookToLibrary();
+lib.deleteBook();
+lib.changeStatus();
 
 
-addButton.addEventListener("click",function(){
+
+addButton.addEventListener("click", function () {
     form.classList.toggle("display-form");
 
 })
-
-
-
-/*
-inputGroup.addEventListener("click",function(event){
-    if(event.target.matches("#add")){
-        
-    }
-
-})
-
-
-*/
-
-
-/*
-
-let myLibrary = [];
-let book1 = new Book("calleja");
-
-myLibrary.push(book1);
-myLibrary[myLibrary.indexOf(book1)].index=myLibrary.indexOf(book1);
-
-class CardContainer{
-    constructor(myLibrary){
-        this.myLibrary =myLibrary;
-    }
-    delete
-}
-*/
-
-
-
-
-
-
-
 
 
